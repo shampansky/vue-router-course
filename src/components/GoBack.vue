e270868852ec1ca82c5427d8d79d949c99237892
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-09-20 16:58:59 +0300

    Добавление кнопки назад

---
+++ src/components/GoBack.vue
@@ -0,0 +1,11 @@
+<template>
+  <span class="go-back">
+    <button @click="router.back()">go back</button>
+  </span>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,6 +1,7 @@
 <template>
 <section v-if="destination" class="destination">
     <h1>{{ destination.name }}</h1>
+    <GoBack />
     <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name">
       <p>{{ destination.description }}</p>
@@ -26,6 +27,7 @@
 <script setup>
 import sourceData from '@/data.json'
 import ExperienceCard from '@/components/ExperienceCard.vue'
+import GoBack from '@/components/GoBack.vue'
 // import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 
