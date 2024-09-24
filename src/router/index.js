53aa19b9d10ca0ec9700cb164fb880b9d0d67aa4
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-09-24 14:58:06 +0300

    Per route navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   {
@@ -12,6 +13,14 @@ const routes = [
     name: 'destination.show',
     component: () => import('@/views/DestinationShow.vue'),
     props: route => ({...route.params, id: parseInt(route.params.id)}),
+    beforeEnter(to) {
+      console.log(to)
+      const exists = sourceData.destinations.find(item => item.id === +to.params.id);
+      if (!exists) return {
+        name: 'NotFound',
+        params: { pathMatch: to.path.split('/').slice(1), query: to.query, hash: to.hash },
+      }
+    },
     children: [
       {
         path: ':experienceSlug',
