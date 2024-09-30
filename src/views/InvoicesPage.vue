75001fd9757b23fef1aece0304f08299f2cda68a
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-09-30 06:32:48 +0300

    хук onBeforeRouteLeave

--- src/views/InvoicesPage.vue
+++ src/views/InvoicesPage.vue
@@ -1,5 +1,10 @@
 <script setup>
+  import { onBeforeRouteLeave } from 'vue-router'
 
+  onBeforeRouteLeave(() => {
+    const answer = window.confirm('Are you sure you want to leave?')
+    if (!answer) return false
+  })
 </script>
 
 <template>
